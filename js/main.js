3d63a077914de13a573fe4a260bdc61bd51075b8
dssff <dendssff@gmail.com>
2023-10-29 19:38:15 +0200

    all txt files were added to the project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){ 
+   $('.design-slider').slick({ 
+     dots: false, 
+     slidesToShow: 4, 
+     variableWidth: true, 
+     prevArrow:'<img class= "arrow arrow-left" src="images/arrow 2.png" alt="">', 
+     nextArrow:'<img class= "arrow arrow-right" src="images/arrow 1.png" alt="">' 
+   }); 
+   });
